2fb53b632f3964ca0921a3bb7436edf17dd314d7
acamacho88 <billyshears06@gmail.com>
2019-02-18 00:01:09 +0000

    have basic footer and font awesome source

--- components/Footer.js
+++ components/Footer.js
@@ -1,6 +1,38 @@
-const Footer = () => (
-    <div>
+const footerStyling = {
+    backgroundColor: "#212222",
+    height: "175px",
+    width: "100%",
+    display: "flex",
+    flexDirection: "row",
+    boxShadow: "inset 0 1px 3px 0 rgba(0,0,0,0.5), 0 2px 4px 0 rgba(0,0,0,0.5)"
+}
 
+const footerInnerStyling = {
+    color: "#D8D8D8",
+    fontSize: "18px",
+    fontFamily: "Avenir Next",
+    lineHeight: "25px",
+    fontWeight: "500",
+    width: "40%"
+}
+
+const footerSocMedStyling = {
+    textAlign: "right"
+}
+
+const footerEmptyStyling = {
+    width: "30%"
+}
+
+const Footer = () => (
+    <div style={footerStyling}>
+        <div style={footerInnerStyling}>C 2019 Pure Plumbing Solutions. All rights reserved.</div>
+        <div style={footerEmptyStyling}></div>
+        <div style={footerSocMedStyling}>
+            <i className="fab fa-facebook-f"></i>
+            <i className="fab fa-instagram"></i>
+            <i className="fab fa-linkedin-in"></i>
+        </div>
     </div>
 )
 
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,10 @@
+import Footer from "../components/Footer";
+
 const Index = () => (
     <div>
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous"></link>
         <p>Hello Next.js</p>
+        <Footer />
     </div>
 )
   
