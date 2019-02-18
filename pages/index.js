2622376eac6722bfc47321b775d6b55575a925d2
Henry Yi <henryyi@Henrys-MacBook-Pro.local>
2019-02-17 16:45:57 -0800

    jumbotron home page

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,42 @@
 import Layout from '../components/Layout';
 const Index = () => (
     <Layout>
-        <p>Index Page (HOME)</p>
+        <div className="jumbotron">
+            <div className="cta-text">Pure Honesty, Solutions, and Satisfaction</div>
+            <div className="cta-button">Call to Action</div>
+        </div>
+        <style jsx>{`
+            .jumbotron {
+                display: flex;
+                flex-direction: column;
+                justify-content: center;
+                width: 100%;
+                height: 420px;
+                background-color: #D8D8D8;
+            }
+
+            .cta-text {
+                margin-left: 112px;
+                color: #FFFFFF;	
+                font-family: "Avenir Next";	
+                font-size: 35px;	
+                font-weight: 500;	
+                line-height: 48px;
+            }
+
+            .cta-button {
+                margin-left: 112px;	
+                height: 75px;	
+                width: 319px;	
+                background-color: #2E69A5;
+                color: #FFFFFF;
+                font-family: "Avenir Next";
+                font-size: 24px;
+                font-weight: 500;
+                line-height: 48px;
+                text-align: center;
+            }
+        `}</style>
     </Layout>
 )
   
