5f25a4d57be04ae5aec0126498bed9932651164e
acamacho88 <billyshears06@gmail.com>
2019-02-18 00:12:11 +0000

    removed hello world p element

--- pages/index.js
+++ pages/index.js
@@ -6,7 +6,6 @@ const Index = () => (
         <Header />
         <p>Index Page (HOME)</p>
         <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous"></link>
-        <p>Hello Next.js</p>
         <Footer />
     </div>
 )
