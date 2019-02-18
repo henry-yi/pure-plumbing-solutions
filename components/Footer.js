349c379a3f600aeee2c19263ca27f541e2f75f01
acamacho88 <billyshears06@gmail.com>
2019-02-18 00:34:59 +0000

    converted css objects to jsx

--- components/Footer.js
+++ components/Footer.js
@@ -1,48 +1,47 @@
-const footerStyling = {
-    backgroundColor: "#212222",
-    height: "175px",
-    width: "100%",
-    display: "flex",
-    flexDirection: "row",
-    boxShadow: "inset 0 1px 3px 0 rgba(0,0,0,0.5), 0 2px 4px 0 rgba(0,0,0,0.5)"
-}
-
-const footerInnerStyling = {
-    color: "#D8D8D8",
-    fontSize: "18px",
-    fontFamily: "Avenir Next",
-    lineHeight: "25px",
-    fontWeight: "500",
-    width: "40%"
-}
-
-const footerSocMedStyling = {
-    textAlign: "right"
-}
-
-const footerEmptyStyling = {
-    width: "30%"
-}
-
-const socMedIconStyling = {
-    margin: "5px"
-}
-
 const Footer = () => (
-    <div style={footerStyling}>
-        <div style={footerInnerStyling}>C 2019 Pure Plumbing Solutions. All rights reserved.</div>
-        <div style={footerEmptyStyling}></div>
-        <div style={footerSocMedStyling}>
-            <a style={socMedIconStyling} href="#">
+    <div className="footer">
+        <div className="footer__inner">C 2019 Pure Plumbing Solutions. All rights reserved.</div>
+        <div className="social_media_block">
+            <a className="social_media_icon" href="#">
                 <i className="fab fa-facebook-f"></i>
             </a>
-            <a style={socMedIconStyling} href="#">
+            <a className="social_media_icon" href="#">
                 <i className="fab fa-instagram"></i>
             </a>
-            <a style={socMedIconStyling} href="#">
+            <a className="social_media_icon" href="#">
                 <i className="fab fa-linkedin-in"></i>
             </a>
         </div>
+        <style jsx>{`
+            .footer {
+                background-color: #212222;
+                height: 175px;
+                width: 100%;
+                display: flex;
+                flex-direction: row;
+                justify-content: space-between;
+                box-shadow: inset 0 1px 3px 0 rgba(0,0,0,0.5), 0 2px 4px 0 rgba(0,0,0,0.5);
+            }    
+
+            .footer__inner {
+                color: #D8D8D8;
+                font-size: 18px;
+                font-family: "Avenir Next";
+                line-height: 25px;
+                font-weight: 500;
+                width: 40%;
+            }
+
+            .social_media_block {
+                text-align: right;
+            }
+
+            .social_media_icon {
+                margin: 5px;
+            }
+    
+        `
+        }</style>
     </div>
 )
 
