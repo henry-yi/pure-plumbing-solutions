192049c8583a63ae7394a609583be845cc9aeb8c
Henry Yi <henryyi@Henrys-MacBook-Pro.local>
2019-02-17 16:13:37 -0800

    fixing index page

--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,8 @@
-<<<<<<< HEAD
 import Layout from '../components/Layout';
-=======
-import Footer from "../components/Footer";
-import Header from '../components/Header';
-
->>>>>>> d3d1c5dfd32194eaf519ca306d055787b545229c
 const Index = () => (
     <Layout>
         <p>Index Page (HOME)</p>
-<<<<<<< HEAD
     </Layout>
-=======
-        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous"></link>
-        <p>Hello Next.js</p>
-        <Footer />
-    </div>
->>>>>>> d3d1c5dfd32194eaf519ca306d055787b545229c
 )
   
 export default Index
