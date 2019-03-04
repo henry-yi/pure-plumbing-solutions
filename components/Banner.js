5fe7c2d56c272a165c0060847bcfc6079aaa8129
Henry Yi <henryyi@Henrys-MacBook-Pro.local>
2019-03-03 16:21:11 -0800

    banner component css for all screen sizes, about page

--- components/Banner.js
+++ components/Banner.js
@@ -5,11 +5,41 @@ const Banner = (props) => (
         </div>
         <style jsx>{`
             @media (min-width: 320px) and (max-width: 1023px) {
+                .banner-image {
+                    height: 96px;
+                    background: gray;
+                    display: flex;
+                    justify-content: flex-start;
+                    align-items: center;
+                }
 
+                .banner-text {
+                    margin-left: 12px;
+                    color: #FFFFFF;	
+                    font-family: "Avenir Next";	
+                    font-size: 24px;	
+                    font-weight: 500;	
+                    line-height: 24px;                    
+                }
             }
 
             @media (min-width: 1024px) {
+                .banner-image {
+                    height: 180px;
+                    background: gray;
+                    display: flex;
+                    justify-content: flex-start;
+                    align-items: center;
+                }
 
+                .banner-text {
+                    margin-left: 60px;
+                    color: #FFFFFF;	
+                    font-family: "Avenir Next";	
+                    font-size: 30px;	
+                    font-weight: 500;	
+                    line-height: 48px;                    
+                }
             }
 
             @media (min-width: 1440px) {
--- pages/about.js
+++ pages/about.js
@@ -4,17 +4,107 @@ import Layout from '../components/Layout';
 const About = () => (
     <Layout>
         <Banner title="About Pure Plumbing Solutions"/>
+        <div className="about-container">
+            <div className="key-point">
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus non nunc libero. Donec scelerisque ultrices mattis. Aliquam sapien ligula, eleifend in magna vel, condimentum accumsan magna. Aliquam fringilla elementum ex, in eleifend ipsum vulputate non.</p>
+            </div>
+            <div className="key-description">
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus non nunc libero. Donec scelerisque ultrices mattis. Aliquam sapien ligula, eleifend in magna vel, condimentum accumsan magna. Aliquam fringilla elementum ex, in eleifend ipsum vulputate non. Ut malesuada facilisis posuere. Phasellus ut ligula ultricies justo malesuada tristique id eget eros.</p>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras auctor turpis in lorem consequat accumsan. Vestibulum dictum egestas felis vel consectetur. Interdum et malesuada fames ac ante ipsum primis in faucibus. Pellentesque sodales sit amet massa convallis rhoncus. Maecenas eu ullamcorper dui. Integer ornare dolor pharetra nibh dapibus posuere. Vestibulum ultricies vestibulum sagittis. Sed arcu leo, vestibulum at quam vitae, consequat auctor purus.</p>
+            </div>
+        </div>
         <style jsx>{`
             @media (min-width: 320px) and (max-width: 1023px) {
+                .about-container {
+                    display: flex;
+                }
 
+                .key-point {
+                    margin-left: 12px;
+                    margin-right: 14px;
+                    width: 50%;
+                }
+
+                .key-description {
+                    margin-right: 12px;
+                    margin-left: 14px;
+                    width: 50%;
+                }
+
+                .key-description p {
+                    font-family: "Avenir Next";
+                    font-size: 18px;
+                    line-height: 24px;                    
+                }
+
+                .key-point p {
+                    font-family: "Avenir Next";
+                    font-size: 18px;
+                    font-weight: 500;
+                    line-height: 24px;                    
+                }
             }
 
             @media (min-width: 1024px) {
+                .about-container {
+                    display: flex;
+                }
 
+                .key-point {
+                    margin-left: 60px;
+                    margin-right: 20px;
+                    width: 50%;
+                }
+
+                .key-description {
+                    margin-right: 60px;
+                    margin-left: 20px;
+                    width: 50%;
+                }
+
+                .key-description p {
+                    font-family: "Avenir Next";
+                    font-size: 20px;
+                    line-height: 24px;                    
+                }
+
+                .key-point p {
+                    font-family: "Avenir Next";
+                    font-size: 20px;
+                    font-weight: 500;
+                    line-height: 24px;                    
+                }
             }
 
             @media (min-width: 1440px) {
+                .about-container {
+                    display: flex;
+                }
 
+                .key-point {
+                    margin-left: 112px;
+                    margin-right: 28px;
+                    width: 50%;
+                }
+
+                .key-description {
+                    margin-right: 112px;
+                    margin-left: 28px;
+                    width: 50%;
+                }
+
+                .key-description p {
+                    font-family: "Avenir Next";
+                    font-size: 24px;
+                    line-height: 24px;                    
+                }
+
+                .key-point p {
+                    font-family: "Avenir Next";
+                    font-size: 24px;
+                    font-weight: 500;
+                    line-height: 24px;                    
+                }
             }
         `}</style>
     </Layout>
