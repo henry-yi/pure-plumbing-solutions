374c48a69f79ab02d48795b432e9476f741199a0
Henry Yi <yoonho.yi88@gmail.com>
2019-08-25 16:41:53 -0700

    updating navheader to new v1

--- components/Layout.js
+++ components/Layout.js
@@ -6,7 +6,6 @@ const Layout = (props) => (
     <div>
         <Head>
             <title>Pure Plumbing Solutions</title>
-            <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossOrigin="anonymous"></link>
             <meta name="viewport" content="initial-scale=1.0, width=device-width" />
         </Head>
         <Header />
