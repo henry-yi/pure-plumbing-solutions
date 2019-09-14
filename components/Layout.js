8559e5169a7b4495c557595ed63d67db391b48a7
Henry Yi <yoonho.yi88@gmail.com>
2019-09-14 16:37:11 -0700

    adding wrapper on index

--- components/Layout.js
+++ components/Layout.js
@@ -7,12 +7,12 @@ const Layout = (props) => (
         <Head>
             <title>Pure Plumbing Solutions</title>
             <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossOrigin="anonymous"></link>
-            <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+            <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         </Head>
         <Header />
         { props.children }
         <Footer />
-        <style jsx global>{`
+        <style jsx global>{`     
             html {
                 scroll-behavior: smooth;
             }
--- pages/index.js
+++ pages/index.js
@@ -5,39 +5,45 @@ import Link from 'next/link';
 
 const Index = () => (
     <Layout>
-        <div className="jumbotron">
-            <div className="cta-box">
-                <div className="cta-text">Need A Plumber?<br/>We Can Help.</div>
+        <div className="wrapper">
+            <div className="jumbotron">
+                <div className="cta-box">
+                    <div className="cta-text">Need A Plumber?<br/>We Can Help.</div>
+                    <Link href="/contact">
+                        <div className="cta-button">Get Free Quote</div>
+                    </Link>
+                </div>
+            </div>
+            <div className="promises">
+                <div className="business-points">
+                    <div className="business-points__title">Our Promise</div>
+                    <div className="business-points__bullets">
+                        <BusinessPoint source="index" title="Honest Advice" description="We promise to only give our honest assessments. If we don’t think it’s broken, we won’t tell you to fix it." />
+                        <BusinessPoint source="index" title="Punctual Service" description="We know you’re busy. Thats why we’ll always arrive within the hour of your scheduled time." />
+                        <BusinessPoint source="index" title="Solution Guaranteed" description="We’ll do our best to fix your issue but if we can’t, we won’t charge a dime, guaranteed." />
+                    </div>
+                </div>
+            </div>
+
+            <div className="helpCards">
+                <div className="helpSectionTitle">Our Services</div>
+                <div className="helpCardBox">
+                    <HowCanWeHelpCard destination="waterheater" ico="fire" title="Water Heater" items={["Installation","Repairs","Routine Maintenance","Products"]}/>
+                    <HowCanWeHelpCard destination="seweranddrain" ico="wrench" title="Sewer & Drain" items={["Drain Cleaning","Sewer Repairs","Main Line Installation","Underfloor Drain Line Inspection"]}/>
+                    <HowCanWeHelpCard destination="gas" ico="burn" title="Gas" items={["Pipe Installation","Leak Detection","Repairs","Appliance Hook Up"]}/>
+                    <HowCanWeHelpCard destination="plumbingfixtures" ico="toilet" title="Fixtures" items={["Installation & Repairs","Faucets","Toilets","Sinks","Showers"]}/>
+                    <HowCanWeHelpCard destination="homeimprovementplan" ico="tasks" title="Home Improvement" items={["Annual Maintenance Plan","Pure Satisfaction","Honesty","Solutions"]}/>
+                </div>
                 <Link href="/contact">
                     <div className="cta-button">Get Free Quote</div>
                 </Link>
             </div>
         </div>
-        <div className="promises">
-            <div className="business-points">
-                <div className="business-points__title">Our Promise</div>
-                <div className="business-points__bullets">
-                    <BusinessPoint source="index" title="Honest Advice" description="We promise to only give our honest assessments. If we don’t think it’s broken, we won’t tell you to fix it." />
-                    <BusinessPoint source="index" title="Punctual Service" description="We know you’re busy. Thats why we’ll always arrive within the hour of your scheduled time." />
-                    <BusinessPoint source="index" title="Solution Guaranteed" description="We’ll do our best to fix your issue but if we can’t, we won’t charge a dime, guaranteed." />
-                </div>
-            </div>
-        </div>
-
-        <div className="helpCards">
-            <div className="helpSectionTitle">Our Services</div>
-            <div className="helpCardBox">
-                <HowCanWeHelpCard destination="waterheater" ico="fire" title="Water Heater" items={["Installation","Repairs","Routine Maintenance","Products"]}/>
-                <HowCanWeHelpCard destination="seweranddrain" ico="wrench" title="Sewer & Drain" items={["Drain Cleaning","Sewer Repairs","Main Line Installation","Underfloor Drain Line Inspection"]}/>
-                <HowCanWeHelpCard destination="gas" ico="burn" title="Gas" items={["Pipe Installation","Leak Detection","Repairs","Appliance Hook Up"]}/>
-                <HowCanWeHelpCard destination="plumbingfixtures" ico="toilet" title="Fixtures" items={["Installation & Repairs","Faucets","Toilets","Sinks","Showers"]}/>
-                <HowCanWeHelpCard destination="homeimprovementplan" ico="tasks" title="Home Improvement" items={["Annual Maintenance Plan","Pure Satisfaction","Honesty","Solutions"]}/>
-            </div>
-            <Link href="/contact">
-                <div className="cta-button">Get Free Quote</div>
-            </Link>
-        </div>
         <style jsx>{`
+            .wrapper {
+                overflow-x: hidden;
+            }
+
             .promises {
                 background: url('../static/wave.svg') bottom center;
                 background-size: 100%;
